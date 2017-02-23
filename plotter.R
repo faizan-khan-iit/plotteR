21bac1128315af7c60d23d633035629253f4ccb4
faizan <faizan.khan.iitbhu@gmail.com>
2017-02-23 15:30:05 +0530

    basic data editing

--- plotter.R
+++ plotter.R
@@ -1,10 +1,19 @@
-plotter <- function(inp_data = NULL, graphType = NULL){
+plotter <- function(inp_data = NULL, graphType = NULL,
+                    graphOptions = list()){
   if(is.null(inp_data)){
     stop("No input data provided")
   }
   
+  if(is.null(graphType)){
+    graphType <- "scatter"
+  }
+  if(length(graphOptions) < 1){
+    graphOptions <- list(x="x",
+                         y="y")
+  }
   data_clean <- cleanData(inp_data)
-  data_build <- editData(data_clean, graphType)
+  data_build <- editData(data_clean, graphType, graphOptions)
+  browser()
   saveData(data_build)
   printMessage()
   return(invisible(NULL))
@@ -14,8 +23,27 @@ cleanData <- function(inp_data){
   inp_data
 }
 
-editData <- function(data_clean, graphType){
+editData <- function(data_clean, graphType, graphOptions){
   data_clean
+  ans <- data.frame(x = data_clean[[graphOptions$x]],
+                    y = data_clean[[graphOptions$y]])
+  if(is.null(graphOptions$xlab)){
+    graphOptions$xlab <- graphOptions$x
+  }
+  if(is.null(graphOptions$ylab)){
+    graphOptions$ylab <- graphOptions$y
+  }
+  labs <- list()
+  labs$xlab <- graphOptions$xlab
+  labs$ylab <- graphOptions$ylab
+  
+  ranges <- list()
+  ranges$xrange <- range(ans$x, na.rm = T)
+  ranges$yrange <- range(ans$y, na.rm = T)
+  
+  list(ans = ans,
+       labs = labs,
+       ranges = ranges)
 }
 
 saveData <- function(data_build){
